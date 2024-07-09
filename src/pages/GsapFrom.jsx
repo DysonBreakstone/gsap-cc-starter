00eb675bf6d8fe498fffeccaccbee604442695ee
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2024-07-09 14:06:19 -0600

    gsap from

--- src/pages/GsapFrom.jsx
+++ src/pages/GsapFrom.jsx
@@ -1,4 +1,18 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapFrom = () => {
+  useGSAP(() => {
+    gsap.from('#green-box', {
+      x:250,
+      repeat: -1,
+      yoyo: true,
+      rotation: 360,
+      duration: 2,
+      ease: 'power1.inOut',
+    })
+  }, []);
+
   // TODO: Implement the gsap.from() method
 
   return (
